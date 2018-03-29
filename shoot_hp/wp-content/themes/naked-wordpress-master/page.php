3cb70f05be9c0aed6f687ac41ee345573520832c
naaaaya <naaya981024@gmail.com>
2018-03-29 20:03:08 +0900

    add style for single page

--- shoot_hp/wp-content/themes/naked-wordpress-master/page.php
+++ shoot_hp/wp-content/themes/naked-wordpress-master/page.php
@@ -5,36 +5,36 @@
  */
 
 get_header(); // This fxn gets the header.php file and renders it ?>
-	<div id="primary" class="row-fluid">
-		<div id="content" role="main" class="span8 offset2">
+	<div id="primary single-page_wrapper" class="row-fluid">
+		<div id="single-page_content" role="main" class="span8 offset2">
 
-			<?php if ( have_posts() ) : 
+			<?php if ( have_posts() ) :
 			// Do we have any posts/pages in the databse that match our query?
 			?>
 
-				<?php while ( have_posts() ) : the_post(); 
+				<?php while ( have_posts() ) : the_post();
 				// If we have a page to show, start a loop that will display it
 				?>
 
 					<article class="post">
-					
-						<h1 class="title"><?php the_title(); // Display the title of the page ?></h1>
-						
+
+						<h1 class="single-page_title"><?php the_title(); // Display the title of the page ?></h1>
+
 						<div class="the-content">
-							<?php the_content(); 
+							<?php the_content();
 							// This call the main content of the page, the stuff in the main text box while composing.
 							// This will wrap everything in p tags
 							?>
-							
+
 							<?php wp_link_pages(); // This will display pagination links, if applicable to the page ?>
 						</div><!-- the-content -->
-						
+
 					</article>
 
 				<?php endwhile; // OK, let's stop the page loop once we've displayed it ?>
 
 			<?php else : // Well, if there are no posts to display and loop through, let's apologize to the reader (also your 404 error) ?>
-				
+
 				<article class="post error">
 					<h1 class="404">Nothing posted yet</h1>
 				</article>
