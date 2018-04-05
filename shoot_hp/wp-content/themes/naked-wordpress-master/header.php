89bc38b643c5a141c1d46f9cd4dcd31ccc70bca4
naaaaya <naaya981024@gmail.com>
2018-04-05 15:29:05 +0900

    fixed navbar

--- shoot_hp/wp-content/themes/naked-wordpress-master/header.php
+++ shoot_hp/wp-content/themes/naked-wordpress-master/header.php
@@ -16,6 +16,7 @@
 <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
 <?php // We are loading our theme directory style.css by queuing scripts in our functions.php file,
 	// so if you want to load other stylesheets,
@@ -46,33 +47,30 @@
 >
 
 <header id="masthead" class="site-header">
-
-		<nav class="navbar navbar-default bg-white">
-			<div class="container header-navbar">
-					<a class="logo_name clearfix" href="http://shootsummerschool.org/">
-						<img alt="logo" src="http://shootsummerschool.org/wp-content/themes/naked-wordpress-master/assets/images/logo3_green_oval.png" width="60px" height="60px" class=''>
-						<div class='brand-name pull-right'>
-							SHOOT</br>SUMMER</br>SCHOOL
-						</div>
-					</a>
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
-					<?php
-					wp_nav_menu( array(
-							'theme_location'    => 'primary',
-							'depth'             => 2,
-							'container'         => 'div',
-							'container_id'      => 'bs-example-navbar-collapse-1',
-							'menu_class'        => 'nav navbar-nav',
-							'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-							'walker'            => new WP_Bootstrap_Navwalker()
-						) );
-					?>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="container header-navbar">
+			<a class="logo_name clearfix" href="http://shootsummerschool.org/">
+				<img alt="logo" src="http://shootsummerschool.org/wp-content/themes/naked-wordpress-master/assets/images/logo3_green_oval.png" width="60px" height="60px" class=''>
+				<div class='brand-name float-right'>
+					SHOOT</br>SUMMER</br>SCHOOL
 				</div>
-		</nav>
-
-		<!-- .site-navigation .main-navigation -->
-</header><!-- #masthead .site-header -->
+			</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+				<?php
+				wp_nav_menu( array(
+						'theme_location'    => 'primary',
+						'depth'             => 1,
+						'menu_class'        => 'nav navbar-nav float-right',
+						'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+						'walker'            => new WP_Bootstrap_Navwalker()
+					) );
+				?>
+		  </div>
+		</div>
+	</nav>
+</header>
 
 <main class="main-container"><!-- start the page containter -->
